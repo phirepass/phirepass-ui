665f6bd9d3190c94a918e871f09f07bc57ea35f1
dimitrmo <dmouratidis153@gmail.com>
2026-01-24 14:30:12 +0200

    Switch to a single param

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,11 +1,7 @@
 import { Pool, QueryResult } from 'pg';
 
 const pool = new Pool({
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
-    database: process.env.DATABASE_NAME,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
+    connectionString: process.env.DATABASE_URL,
 });
 
 export async function query(text: string, params?: unknown[]): Promise<QueryResult> {
