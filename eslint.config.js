b0023b5db723259189e35e7906408b2265528013
dimitrmo <dmouratidis153@gmail.com>
2026-03-22 18:37:36 +0200

    Add title

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,13 @@ export default tseslint.config(
     },
     rules: {
     ...reactHooks.configs.recommended.rules,
-    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "react-refresh/only-export-components": [
+      "warn",
+      {
+        allowConstantExport: true,
+        allowExportNames: ["metadata", "generateMetadata", "viewport"],
+      },
+    ],
     "@typescript-eslint/no-unused-vars": "off",
     },
   },
