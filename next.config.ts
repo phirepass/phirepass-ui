8b66bcea5428936d1bbfc57929427059ca34b432
dimitrmo <dmouratidis153@gmail.com>
2026-03-25 18:47:46 +0200

    Updates

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
     reactStrictMode: true,
     output: 'standalone',
     turbopack: {
+        root: __dirname,
         resolveAlias: {
             'phirepass-channel_bg.wasm': 'phirepass-channel/phirepass-channel_bg.wasm',
         },
